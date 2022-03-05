1d1479b2821cf13cb478582291e567e287500d66
Jason Vance <jbvance@gmail.com>
2022-03-04 20:00:19 -0600

    add RecipeService

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,23 @@
+import { EventEmitter } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a test',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272'
+    ),
+    new Recipe(
+      'Another recipe',
+      'This one is good',
+      'https://img.delicious.com.au/Whdi4i8g/w759-h506-cfill/del/2021/05/slow-roasted-butter-eggplant-curry-152139-2.jpg'
+    ),
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
