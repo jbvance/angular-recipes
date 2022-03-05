1ef5e2f9a9c219b85df9e87267c57a0cf0531ea6
Jason Vance <jbvance@gmail.com>
2022-03-04 21:00:55 -0600

    update recipe detail component to show ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
@@ -6,14 +7,16 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is a test',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272'
+      'Tasty Schnitzel',
+      'A super tasty schitzel - just awesome!',
+      'https://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-1-1.jpg',
+      [new Ingredient('meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
-      'Another recipe',
-      'This one is good',
-      'https://img.delicious.com.au/Whdi4i8g/w759-h506-cfill/del/2021/05/slow-roasted-butter-eggplant-curry-152139-2.jpg'
+      'Big Fat Burger',
+      'What else do you need to say?',
+      'https://assets.epicurious.com/photos/57c5c6d9cf9e9ad43de2d96e/master/w_1280,c_limit/the-ultimate-hamburger.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
     ),
   ];
 
