0d3e2a29c00c93dc9e68f7eba65a4286e403beb8
Jason Vance <jvance@NSLLP.local>
2022-03-16 15:57:43 -0500

    add signup functionality

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseApiToken: 'AIzaSyBdvSdJIDiz2UP8yAGTfcBW0PRlRK8KvJY',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseApiToken: 'AIzaSyBdvSdJIDiz2UP8yAGTfcBW0PRlRK8KvJY',
 };
 
 /*
